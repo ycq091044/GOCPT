d430fa31b2500329c0f221a646a5b734aca8b0fe
ycq091044 <ycqsjtu@gmail.com>
2022-04-21 23:35:53 -0500

    update help page and add setup.py

--- GOCPT/__init__.py
+++ GOCPT/__init__.py
@@ -1,5 +1,35 @@
 import sys
+
+# README
+README = """
+---------------------------- Help ---------------------------
+GOCPT is a python open-source package for handing generalized 
+online tensor decomposition and completion.
+
+We provide the following functions:
+
+    - for online tensor factorization of arbitraty tensors
+        - GOCPT.otf.OnlineCPD
+        - GOCPT.otf.MAST
+        - GOCPT.otf.CPStream
+        - GOCPT.otf.GOCPTE
+
+    - for online tensor factorization of 3-order tensors
+        - GOCPT.otf.SDT
+        - GOCPT.otf.RLST
+
+    - for online tensor completion of arbitraty tensors
+        - GOCPT.otc.OnlineSGD
+        - GOCPT.otc.OLSTEC
+        - GOCPT.otc.GOCPTE
+
+EXAMPLE:
+    >> from GOCPT import otf, datasets
+    >> X = datasets.GCSS()
+    >> model = otf.GOCPTE(base_X=X, R=5, iters=50)
+    >> new_slice = X[...,-2:-1]
+    >> model.update(new_slice, verbose=True) 
+"""
+
 def GOCPT():
-    print (" ----- HELP -----\n")
-    print (">> python3\n")
-    print (">> from GOCPT import otc, otf")
+    print (README)
